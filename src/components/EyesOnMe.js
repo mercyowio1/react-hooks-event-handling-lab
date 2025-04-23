b185dd1d34f216e1cdc5806b8a59a7a37a152e5c
Mercy <owinomercy983@gmail.com>
2025-04-23 20:19:26 +0300

    event lab

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,26 @@
 // Code EyesOnMe Component Here
+// Code EyesOnMe Component Here
+import React from "react";
+
+function EyesOnMe(){
+    const handleFocus = () => {
+        console.log('Good!');
+      };
+    
+      const handleBlur = () => {
+        console.log('Hey! Eyes on me!');
+      };
+
+
+    return (
+        <>
+        
+        <div>
+      <button onFocus={handleFocus} onBlur={handleBlur}>
+        Eyes on me
+      </button>
+      </div>
+        </>
+    )
+}
+export default EyesOnMe
